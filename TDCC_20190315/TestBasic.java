427452febf643ea50ce49c1cc7e9f187f4a5830c
JamesSyu <N1046728@ntub.edu.tw>
2019-03-17 22:12:28 +0800

    Create TestBasic.java

---
+++ TDCC_20190315/TestBasic.java
@@ -0,0 +1,25 @@
+
+public class TestBasic {
+	public static void main(String args[]) {
+		int[] a = new int[10];
+		System.out.println(a[9]); //print 0 --> mean default =0
+		
+		int b = 0; //print 0 1
+		System.out.println(b++);
+		System.out.println(b);
+		
+		int c = 2; //print BC
+		switch(c) {
+			case 1:
+				System.out.println("A");
+			case 2:
+				System.out.println("B");
+			case 3:
+				System.out.println("C");
+				break;
+			default:
+				System.out.println("default");
+		}
+	}
+}
+
