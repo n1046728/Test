f56f3ffcc42a798235629e4492d21c1cde8c9a06
JamesSyu <N1046728@ntub.edu.tw>
2019-07-09 09:21:41 +0800

    close

--- src/TAIFEX_201001/TestInheritance.java
+++ src/TAIFEX_201001/TestInheritance.java
@@ -6,5 +6,44 @@ package TAIFEX_201001;
 //polygon. (Please use JAVA program) 10
  
 public class TestInheritance {
-
+	public static void main(String args[]) {
+		Polygon rectangle = new Rectangle(10.0,5.0);
+		Polygon triangle = new Triangle(5,10);
+		System.out.println("rectangle area :"+rectangle.area());
+		System.out.println("triangle area :"+triangle.area());
+		
+	}
 }
+abstract class Polygon{
+	abstract double area();
+}
+class Rectangle extends Polygon{
+	private double len;
+	private double width;
+	
+	public Rectangle(double len ,double width) {
+		this.len = len;
+		this.width = width;
+	}
+	@Override
+	double area() {
+		return len * width;
+	}
+	
+}
+
+class Triangle extends Polygon{
+	private double bottom ;
+	private double high;
+	
+	public Triangle(double bottom ,double high) {
+		this.high = high;
+		this.bottom = bottom;
+	}
+	@Override
+	double area() {
+		return bottom*high*0.5;
+	}
+	
+}
+
