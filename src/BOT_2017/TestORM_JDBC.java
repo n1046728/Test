b757b242c69bf1535648358571289f2ea919b72b
JamesSyu <N1046728@ntub.edu.tw>
2019-08-27 17:25:29 +0800

    Create TestORM_JDBC.java

---
+++ src/BOT_2017/TestORM_JDBC.java
@@ -0,0 +1,29 @@
+package BOT_2017;
+//第一題
+//EmpFile 資料庫的 Employee 資料表中原本儲存的人員薪資如下表所示。請為經理
+//( 加薪 2000 元，副經理 (Deputy_ 加薪 1500 元，其他員工皆加薪 1000 元，
+//將加薪後的結果寫回 Employee 資料表，完成後將調整後的結果列出。
+//本題程式作答時請以
+//JAVA+SQL 或 .NET C#+SQL 擇一進行 。 【 25 分】
+
+//Employee
+//資料表：	E_NO Name Job_Title Salary
+//		001 Jack Manager 42000
+//		002 Mary Deputy_Manager 40000
+//		003 Tom Staff 36000
+//		004 Nancy Staff 36000
+//		005 Nakamura Staff 36000
+//
+//執行結果：
+//001 Jack Manager 44000.0
+//002 Mary Deputy_Manager 41500.0
+//003 Tom Staff 37000.0
+//004 Nancy Staff 37000.0
+//005 Nakamura Staff 37000.0
+
+public class TestORM_JDBC {
+	public static void main(String []args) {
+		
+		
+	}
+}
